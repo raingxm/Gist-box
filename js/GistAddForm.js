cc3d87bee8ef5d1b50707217cc1a9bb6c58cebb6
raingxm <raingxm@163.com>
2015-12-09 21:46:33 +0800

    finish gistbox

--- js/GistAddForm.js
+++ js/GistAddForm.js
@@ -10,13 +10,14 @@ var GistAddForm = React.createClass({
 	},
 	
 	
-	handleSubmit: function(e) {
+	addGist: function(e) {
 		e.preventDefault();
+		this.props.onAdd(this.state.search);
 	},
 
 	render: function() {
 		return (
-			<form className="form-group" onSubmit={this.handleSubmit}>
+			<form className="form-group" onSubmit={this.addGist}>
 				<input onChange={this.handleChange} className="form-control" placeholder="Type a github name here" 
 					value={this.state.search}/>
 				<button className="btn btn-primary">Fetch latest gist</button>
--- js/GistBox.js
+++ js/GistBox.js
@@ -1,22 +1,27 @@
 var GistBox = React.createClass({
 	getInitialState: function() {
 		return {
-			gists: [
-				{username: 'raingxm', url: 'https://gist.github.com/raingxm/f9b0aaf9c1868d09b42d'}
-			]
+			gists: []
 		};
 	},
 
-	newGist: function(gist) {
-		return <Gist username={gist.username} url={gist.url} />;
+	handleAddGist: function(userName) {
+		var url = 'https://api.github.com/users/' + userName + '/gists';
+		$.get(url, function(data) {
+			this.setState({gists: this.state.gists.concat({username: userName, url: data[0].html_url})});
+		}.bind(this));
 	},
 
 	render: function() {
+		var newGist = function(gist) {
+			return <Gist username={gist.username} url={gist.url} />;	
+		};
+
 		return (
 			<div className='gist-box'>
 				<h1>Gist Box</h1>
-				<GistAddForm />
-				{ this.state.gists.map(this.newGist) }
+				<GistAddForm onAdd={this.handleAddGist} />
+				{ this.state.gists.map(newGist) }
 			</div>
 		);
 	}
